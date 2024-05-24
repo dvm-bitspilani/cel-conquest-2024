8cffc2b857bbece88c99eae1a97a99fb604b5e81
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-24 13:12:16 +0530

    feat: main code in login

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Landing from "./routes/Landing/Landing";
 import Login from "./routes/Login/Login";
+import LandingPage from "./routes/Landing/LandingPage";
 import RootLayout from "./routes/RootLayout/RootLayout";
 
 import WebContextProvider from "./store/website-context";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
-    children: [
-      { path: '/', element: <Landing /> },
-    ]
+    children: [{ path: "/", element: <LandingPage /> }],
   },
-  { path: '/login', element: <Login /> }
-])
+  { path: "/login", element: <Login /> },
+]);
 
 function App() {
   return (
     <WebContextProvider>
       <RouterProvider router={router} />
     </WebContextProvider>
-  )
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='1003752349264-n8tlblfv003sj7pjka1lbtb1sungnvre.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId="1003752349264-n8tlblfv003sj7pjka1lbtb1sungnvre.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
