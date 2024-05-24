cf406ac65ba3a844ae57effe6636330e46153c09
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-24 12:49:31 +0530

    style: add hero section cards

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,10 +1,26 @@
 import BackgroundGridFadeImage from "../../assets/images/Landing Page/Background Grid Fade.png";
+import HeroSectionCard from "./Components/HeroSectionCard/HeroSectionCard";
+import * as styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
   return (
     <>
-      <div>LandingPage</div>
-      <img src={BackgroundGridFadeImage} alt="" />
+      <img className={styles.backgroundGrid} src={BackgroundGridFadeImage} />
+      <div className={styles.pageContainer}>
+        <div className={styles.heroSection}>
+          <div className={styles.heroSectionUpperPart}>
+            <div></div>
+            <div></div>
+          </div>
+          <HeroSectionCard value="75+" property="Investors" />
+          <HeroSectionCard value="₹30L" property="Prize Money" />
+          <HeroSectionCard value="250" property="Mentors" />
+          <HeroSectionCard value="$100k" property="Resources" />
+        </div>
+        <div className={styles.timeline}></div>
+        <div className={styles.investors}></div>
+        <div className={styles.news}></div>
+      </div>
     </>
   );
 };
