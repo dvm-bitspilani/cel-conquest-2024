d5c578d8a4236d4b2dd31eab8e839a3c856aabde
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-25 19:16:45 +0530

    refractor: shift data to landingPageData.js from the component

---
+++ src/routes/Landing/data/landingPageData.js
@@ -0,0 +1,34 @@
+export const heroSectionCardsData = [
+  { value: "75+", property: "Investors" },
+  { value: "₹30L", property: "Prize Money" },
+  { value: "250", property: "Mentors" },
+  { value: "$100k", property: "Resources" },
+];
+
+export const timelineData = [
+  {
+    srno: "01",
+    date: "15th April - 25th May",
+    title: "Registration Begins",
+    para: "Conquest would be accepting registrations from 3rd April and would go on till 31st May. The registration is free of charge, just fill out a simple hassle-free application form, and voila! you're good to go.",
+  },
+  {
+    srno: "02",
+    date: "10th June - 25th July",
+    title: "Online Mentoring Program",
+    para: "Transcending the boundaries of geography and time, our Top 15 startups get an opportunity to receive online mentoring over a 6 week period. Each session revolves around a critical aspect of startup growth, ranging across pitching, fundraising, building MVP, GTM Strategy and finding a product-market fit.",
+    srcolor: "orange",
+  },
+  {
+    srno: "03",
+    date: "29th July - 3rd August",
+    title: "Accelerator Program",
+    para: "The Top 10 startups move to Bangalore for a 7-day accelerator program where they are provided free accommodation and co-working space. We create an environment of co-learning that helps startups grow together at an exponential pace. The program comprises sessions with successful founders, investment workshops by VCs and Angels and networking events.",
+  },
+  {
+    srno: "04",
+    date: "4th August",
+    title: "Demo Day",
+    para: "Demo Day provides startups with the perfect stage to pitch in front of investors, industry leaders, policymakers and media houses to #MakeAMark. The finale is a day where all the stakeholders of the startup ecosystem come together, engage in thought-provoking conversations, share ideas and create solutions.",
+  },
+];
