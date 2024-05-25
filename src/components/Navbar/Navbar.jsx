fae98d2b77f1e1d5fa43cf4bc90c50a4735cb549
Animeshdj <animeshstick.567@gmail.com>
2024-05-26 03:19:56 +0530

    feat: update Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,16 +7,81 @@ export default function Navbar() {
   return (
     <>
       <nav className={styles.navbar}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        <div className={styles.navLinks}>
+          <ul>
+            <li>
+              <div>
+                <Link to="/network">Network</Link>
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 6L8 10L12 6"
+                    stroke="#111213"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className={styles.rectangle}></div>
+              <div className={styles.dropDownGap}></div>
+              <ul className={styles.networks}>
+                <li>Sponsors</li>
+                <li>Partners</li>
+                <li>Jury</li>
+                <li>Mentors</li>
+              </ul>
+            </li>
+            <li>
+              <div>
+                <Link to="/alumni">Alumni</Link>
+              </div>
+              <div className={styles.rectangle}></div>
+            </li>
+            <li>
+              <div>
+                <Link to="/">Media Presence</Link>
+              </div>
+              <div className={styles.rectangle}></div>
+            </li>
+            <li>
+              <div>
+                <Link to="/about">About us</Link>
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 6L8 10L12 6"
+                    stroke="#111213"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className={styles.rectangle}></div>
+              <div className={styles.dropDownGap}></div>
+              <ul className={styles.aboutUs}>
+                <li>Team</li>
+                <li>FAQs</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <img src={logo} className={styles.logo} />
+        <Link className={styles.register} to="/login">
+          <div>Register</div>
+        </Link>
       </nav>
-      <img src={logo} />
     </>
   );
 }
