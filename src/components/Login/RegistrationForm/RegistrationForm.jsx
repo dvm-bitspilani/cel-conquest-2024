ed5a845571dcefe227ef20495bf7d22406dc965c
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-05-26 20:04:27 +0530

    feat: created basic registration form

--- src/components/Login/RegistrationForm/RegistrationForm.jsx
+++ src/components/Login/RegistrationForm/RegistrationForm.jsx
@@ -1,7 +1,43 @@
+import { useFormik } from 'formik';
+
+import { registrationSchema } from './schemas/registrationSchemas';
 import * as styles from './regi.module.scss'
+import TextInput from '../../TextInput/TextInput';
 
 export default function RegistrationForm() {
+    const { values, errors, touched, handleBlur, handleSubmit, handleChange } = useFormik({
+        initialValues: {
+            startup_name: '',
+            web_url: '',
+            team_size: '',
+            city: '',
+            founder_name: '',
+            email: '',
+            phone: '',
+            linkedin_url: ''
+        },
+        validationSchema: registrationSchema,
+        onSubmit: (values, action) => {
+            console.log(values)
+        }
+    })
+
     return (
-        <h1>Form</h1>
+        <form className={styles.register} onSubmit={handleSubmit}>
+            <div className={styles.inputGrid}>
+                <TextInput
+                    name='startup_name'
+                    heading='Startup Name'
+                    changeFn={handleChange}
+                    blurFn={handleBlur}
+                    value={values.startup_name}
+                    error={errors.startup_name}
+                />
+            </div>
+            <footer>
+                <button className={styles.prev}>Previous Page</button>
+                <input type="submit" value="Next Page" className={styles.next} />
+            </footer>
+        </form>
     )
 }
