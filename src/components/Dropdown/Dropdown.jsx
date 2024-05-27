30eecf1449e71a0ae8bf0c5688e48466ee50b3e2
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-05-27 13:55:41 +0530

    feat: Autocomplete dropdown field for state and city

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,10 +1,34 @@
-import { Select, ConfigProvider } from "antd"
+import { Select, AutoComplete, ConfigProvider } from "antd"
 
 import * as styles from './dropdown.module.scss'
 
-export function TextDropdown({ name, heading, changeFn, blurFn, value, error }) {
+export function TextDropdown({ name, heading, changeFn, blurFn, value, error, searchFn, options }) {
     return (
-        <h1>TextDrop</h1>
+        <div className={styles.inputGrp}>
+            <label htmlFor={name}>{heading}</label>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorError: '#EB0E00',
+                        borderRadius: 8,
+                        lineWidth: 1.5
+                    }
+                }}
+            >
+                <AutoComplete
+                    name={name}
+                    id={name}
+                    onChange={changeFn}
+                    onBlur={blurFn}
+                    onSearch={searchFn}
+                    value={value}
+                    status={error ? 'error' : ''}
+                    style={error ? { backgroundColor: '#FFE6E6', width: '100%' } : { width: '100%' }}
+                    options={options}
+                />
+            </ConfigProvider>
+            <span className={styles.errMsg} style={error ? { visibility: 'visible' } : { visibility: 'hidden' }}>*{error}</span>
+        </div>
     )
 }
 
--- src/components/Login/RegistrationForm/RegistrationForm.jsx
+++ src/components/Login/RegistrationForm/RegistrationForm.jsx
@@ -1,16 +1,38 @@
 import { useFormik } from 'formik';
+import { useEffect, useState } from 'react';
+
+import { TextDropdown, NumDropdown } from '../../Dropdown/Dropdown';
+import TextInput from '../../TextInput/TextInput';
+
+import * as styles from './regi.module.scss';
 
 import { registrationSchema } from './schemas/registrationSchemas';
-import * as styles from './regi.module.scss'
-import TextInput from '../../TextInput/TextInput';
-import { TextDropdown, NumDropdown } from '../../Dropdown/Dropdown';
+import statesData from '../../../data/states.json'
 
 export default function RegistrationForm() {
-    const { values, errors, touched, handleBlur, handleSubmit, handleChange, setFieldValue } = useFormik({
+    const [stateOptions, setStateOptions] = useState([])
+    const [allCities, setAllCities] = useState([])
+    const [allStates, setAllStates] = useState([])
+    const [availableCities, setAvailableCities] = useState([]);
+
+    useEffect(() => {
+        const allStatesList = statesData.map((state) => {
+            return ({
+                value: state.name,
+                label: state.name
+            })
+        })
+        setStateOptions(allStatesList)
+        setAllStates(allStatesList)
+    }, [])
+
+
+    const { values, errors, handleBlur, handleSubmit, handleChange, setFieldValue } = useFormik({
         initialValues: {
             startup_name: '',
-            web_url: '',
+            web_url: 'https://',
             team_size: 1,
+            state: '',
             city: '',
             founder_name: '',
             email: '',
@@ -22,6 +44,39 @@ export default function RegistrationForm() {
             console.log(values)
         }
     })
+
+    useEffect(() => {
+        const state = statesData.find(state => state.name === values.state)
+
+        setFieldValue('city', "")
+
+        if (state) {
+            const cities = state.cities.map(city => {
+                return ({
+                    value: city.name,
+                    label: city.name
+                })
+            })
+            setAvailableCities(cities)
+            setAllCities(cities)
+        }
+    }, [values.state])
+
+    function handleCitySearch(value) {
+        const regex = new RegExp(value)
+        setAvailableCities(() => {
+            const newCity = allCities.filter((city) => regex.test(city.value))
+            return newCity
+        })
+    }
+
+    function handleStateSearch(value) {
+        const regex = new RegExp(value)
+        setStateOptions(() => {
+            const newStates = allStates.filter((state) => regex.test(state.value))
+            return newStates
+        })
+    }
 
     return (
         <form className={styles.register} onSubmit={handleSubmit}>
@@ -51,6 +106,26 @@ export default function RegistrationForm() {
                     error={errors.team_size}
                     min={1}
                     max={10}
+                />
+                <TextDropdown
+                    name='state'
+                    heading='State'
+                    changeFn={(newVal) => setFieldValue('state', newVal)}
+                    blurFn={handleBlur}
+                    searchFn={handleStateSearch}
+                    options={stateOptions}
+                    value={values.state}
+                    error={errors.state}
+                />
+                <TextDropdown
+                    name='city'
+                    heading='City'
+                    changeFn={(newVal) => setFieldValue('city', newVal)}
+                    blurFn={handleBlur}
+                    searchFn={handleCitySearch}
+                    options={availableCities}
+                    value={values.city}
+                    error={errors.city}
                 />
             </div>
             <footer>
