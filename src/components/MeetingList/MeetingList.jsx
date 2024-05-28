7d52506d34625a97455c03393bce110db4d1a14e
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-05-28 19:44:38 +0530

    feat: created week-wise meeting list (to be styled)

--- src/components/MeetingList/MeetingList.jsx
+++ src/components/MeetingList/MeetingList.jsx
@@ -5,14 +5,38 @@ import * as styles from './MeetingList.module.scss'
 import avatar from '../../assets/images/Dashboard/demoAvatar.jpeg'
 
 export default function MeetingList() {
-    return (
-        <div className={styles.wrapper}>
-            <MeetingItem
+    const meetList = []
+    for (let i = 0; i < 3; i++) {
+        const listItms = []
+        for (let j = 0; j < 6; j++) {
+            const newItm = (<MeetingItem
                 date='May 24, 2024, 00:30:00'
                 avatar={avatar}
                 mentorName='Bhavesh'
                 duration={30}
-            />
+                key={j}
+            />)
+            listItms.push(newItm)
+        }
+        const newWeek = (<div className={styles.week}>
+            <p className={styles.weekHeading}>This Week</p>
+            {...listItms}
+        </div>)
+        meetList.push(newWeek)
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            {/* <div className={styles.week}>
+                <p className={styles.weekHeading}>This Week</p>
+                <MeetingItem
+                    date='May 24, 2024, 00:30:00'
+                    avatar={avatar}
+                    mentorName='Bhavesh'
+                    duration={30}
+                />
+            </div> */}
+            {...meetList}
         </div>
     )
 }
