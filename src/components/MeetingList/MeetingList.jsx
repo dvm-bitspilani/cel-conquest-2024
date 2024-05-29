26d81bb69625befd6d43da891cbe2765a00136e8
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-29 14:23:36 +0530

    feat: meetings list options

--- src/components/MeetingList/MeetingList.jsx
+++ src/components/MeetingList/MeetingList.jsx
@@ -1,33 +1,37 @@
-import MeetingItem from './MeetingItem/MeetingItem'
+import MeetingItem from "./MeetingItem/MeetingItem";
 
-import * as styles from './MeetingList.module.scss'
+import * as styles from "./MeetingList.module.scss";
 
-import avatar from '../../assets/images/Dashboard/demoAvatar.jpeg'
+import avatar from "../../assets/images/Dashboard/demoAvatar.jpeg";
 
 export default function MeetingList() {
-    const meetList = []
-    for (let i = 0; i < 3; i++) {
-        const listItms = []
-        for (let j = 0; j < 6; j++) {
-            const newItm = (<MeetingItem
-                date='May 24, 2024, 00:30:00'
-                avatar={avatar}
-                mentorName='Bhavesh'
-                duration={30}
-                key={j}
-            />)
-            listItms.push(newItm)
-        }
-        const newWeek = (<div className={styles.week}>
-            <p className={styles.weekHeading}>This Week</p>
-            {...listItms}
-        </div>)
-        meetList.push(newWeek)
+  const meetList = [];
+  for (let i = 0; i < 3; i++) {
+    const listItms = [];
+    for (let j = 0; j < 6; j++) {
+      const newItm = (
+        <MeetingItem
+          date="May 24, 2024, 00:30:00"
+          avatar={avatar}
+          mentorName="Bhavesh"
+          duration={30}
+          key={j}
+        />
+      );
+      listItms.push(newItm);
     }
+    const newWeek = (
+      <div className={styles.week}>
+        <p className={styles.weekHeading}>This Week</p>
+        {...listItms}
+      </div>
+    );
+    meetList.push(newWeek);
+  }
 
-    return (
-        <div className={styles.wrapper}>
-            {/* <div className={styles.week}>
+  return (
+    <div className={styles.wrapper}>
+      {/* <div className={styles.week}>
                 <p className={styles.weekHeading}>This Week</p>
                 <MeetingItem
                     date='May 24, 2024, 00:30:00'
@@ -36,7 +40,7 @@ export default function MeetingList() {
                     duration={30}
                 />
             </div> */}
-            {...meetList}
-        </div>
-    )
+      {...meetList}
+    </div>
+  );
 }
--- src/routes/Dashboard/Meetings/Meetings.jsx
+++ src/routes/Dashboard/Meetings/Meetings.jsx
@@ -1,6 +1,19 @@
+import styles from "./meetings.module.scss";
+
 const Meetings = () => {
   return (
-    <div>Meetings</div>
-  )
-}
-export default Meetings
+    <div className={styles.meetingsContainer}>
+      <div className={styles.meetingsList}>
+        <div className={styles.meetingsListOptionsContainer}>
+          <div className={`${styles.meetingsListOptions} ${styles.active}`}>
+            Upcoming
+          </div>
+          <div className={styles.meetingsListOptions}>Pending</div>
+          <div className={styles.meetingsListOptions}>Past</div>
+        </div>
+      </div>
+      <div className={styles.meetingsDetails}></div>
+    </div>
+  );
+};
+export default Meetings;
