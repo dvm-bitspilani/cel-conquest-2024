676efa1a064aa43fc8e5b4302b991cd62bd00648
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-05-29 15:52:11 +0530

    feat: moved meeting item array out of list component
    - Allows dynamic adding of items based on pending, completed or upcoming

--- src/routes/Dashboard/Home/Home.jsx
+++ src/routes/Dashboard/Home/Home.jsx
@@ -1,11 +1,27 @@
 import MeetingList from "../../../components/MeetingList/MeetingList"
+import MeetingItem from '../../../components/MeetingList/MeetingItem/MeetingItem';
+import avatar from '../../../assets/images/Dashboard/demoAvatar.jpeg'
 
 import * as styles from './home.module.scss'
 
 export default function Home() {
+  const listItms = [];
+  for (let j = 0; j < 6; j++) {
+    const newItm = (
+      <MeetingItem
+        date="May 24, 2024, 00:30:00"
+        avatar={avatar}
+        mentorName="Bhavesh"
+        duration={30}
+        key={j}
+      />
+    );
+    listItms.push(newItm);
+  }
+
   return (
     <div className={styles.container}>
-      <MeetingList />
+      <MeetingList listItms={listItms} />
     </div>
   )
 }
