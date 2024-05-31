e4ec63f404e190d0a1dbf9697d9e1bd2656242fb
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-31 12:47:43 +0530

    fix: add demo avatar to sidebar and fix the top buttons layout

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -2,20 +2,21 @@ import styles from "./sidebar.module.scss";
 
 import dashboard_mountain_circle from "../../../assets/images/Dashboard/dashboard-mountain-circle.png";
 import Button from "./Button/Button";
+import demoAvatar from "../../../assets/images/Dashboard/demoAvatar.jpeg";
 
-import bell from "../../../assets/images/Dashboard/bell.svg"
-import back_arrow from "../../../assets/images/Dashboard/arrow-left.svg"
+import bell from "../../../assets/images/Dashboard/bell.svg";
+import back_arrow from "../../../assets/images/Dashboard/arrow-left.svg";
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
-      <div>
-        <img src={bell} />
+      <div className={styles.headerButtons}>
         <img src={back_arrow} />
+        <img src={bell} />
       </div>
-      <div>
-        <div>
-          <img height={50} src={dashboard_mountain_circle} />
+      <div className={styles.profileSection}>
+        <div className={styles.profileAvatar}>
+          <img src={demoAvatar} />
         </div>
         <p>Welcome back,</p>
         <p>Madhur Jain</p>
