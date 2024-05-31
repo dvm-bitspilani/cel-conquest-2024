e6b3ccc55993c9d92cb37afb7bcf337c971b7e08
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-31 10:29:12 +0530

    feat: slotInputField structure

--- src/components/Dashboard/Meetings/SelectSlots/SelectSlots.jsx
+++ src/components/Dashboard/Meetings/SelectSlots/SelectSlots.jsx
@@ -1,6 +1,15 @@
 import styles from "./SelectSlots.module.scss";
 
 const SelectSlots = () => {
-  return <div>SelectSlots</div>;
+  return (
+    <div>
+      <h3>Select Slots</h3>
+      
+      <div className={styles.addDone}>
+        <button>Add</button>
+        <button>Done</button>
+      </div>
+    </div>
+  );
 };
 export default SelectSlots;
