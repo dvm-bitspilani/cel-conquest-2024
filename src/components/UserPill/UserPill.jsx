23b7c90ea03656270658f25929ed3e95eea32a7b
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-05-31 18:08:01 +0530

    style: fixed aspect ratio of instructor avatar

--- src/components/UserPill/UserPill.jsx
+++ src/components/UserPill/UserPill.jsx
@@ -5,25 +5,27 @@ import * as styles from './pill.module.scss'
 export default function UserPill({ avatar, name }) {
     return (
         <div className={styles.pillbox}>
-            <ConfigProvider
-                theme={{
-                    token: {
-                        lineWidth: 0,
-                    },
-                }}
-            >
-                <Avatar
-                    size={{
-                        xs: 20,
-                        sm: 26,
-                        md: 34,
-                        lg: 34,
-                        xl: 34,
-                        xxl: 34,
+            <div className={styles.pfp}>
+                <ConfigProvider
+                    theme={{
+                        token: {
+                            lineWidth: 0,
+                        },
                     }}
-                    icon={<img src={avatar} alt="icon" />}
-                />
-            </ConfigProvider>
+                >
+                    <Avatar
+                        size={{
+                            xs: 20,
+                            sm: 26,
+                            md: 34,
+                            lg: 34,
+                            xl: 34,
+                            xxl: 34,
+                        }}
+                        icon={<img src={avatar} alt="Icon" />}
+                    />
+                </ConfigProvider>
+            </div>
             <p className={styles.name}>{name}</p>
         </div>
     )
