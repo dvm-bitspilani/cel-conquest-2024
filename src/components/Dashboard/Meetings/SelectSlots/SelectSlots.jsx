30fa39bd3b45e30eb45c924d557d49228a7ab842
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-31 12:18:58 +0530

    feat: slot input field in select slot component

--- src/components/Dashboard/Meetings/SelectSlots/SelectSlots.jsx
+++ src/components/Dashboard/Meetings/SelectSlots/SelectSlots.jsx
@@ -1,9 +1,11 @@
 import styles from "./SelectSlots.module.scss";
+import SlotInputField from "./SlotInputField/SlotInputField";
 
 const SelectSlots = () => {
   return (
     <div className={styles.SelectSlots}>
       <h3>Select Slots</h3>
+      <SlotInputField></SlotInputField>
       <div className={styles.addDone}>
         <button>Add</button>
         <button>Done</button>
