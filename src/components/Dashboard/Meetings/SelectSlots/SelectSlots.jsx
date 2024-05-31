7b7f9fd648ea63731d6ef878cf5fc24494d16827
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-05-31 10:33:42 +0530

    feat: structure for slot input field

--- src/components/Dashboard/Meetings/SelectSlots/SelectSlots.jsx
+++ src/components/Dashboard/Meetings/SelectSlots/SelectSlots.jsx
@@ -4,7 +4,6 @@ const SelectSlots = () => {
   return (
     <div>
       <h3>Select Slots</h3>
-      
       <div className={styles.addDone}>
         <button>Add</button>
         <button>Done</button>
