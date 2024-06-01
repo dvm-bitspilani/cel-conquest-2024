9d74254d5cfb55a23890ea70b0af27aa06b3b36d
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-01 19:46:26 +0530

    feat: add timeSelectButtonHeader and style slotTimingSelector

---
+++ src/components/Dashboard/Meetings/SlotTimingSelector/TimeSelectButtonHeader/TimeSelectButtonHeader.jsx
@@ -0,0 +1,6 @@
+import styles from "./timeSelectButtonHeader.module.scss";
+
+function TimeSelectButtonHeader() {
+  return <div>TimeSelectButtonHeader</div>;
+}
+export default TimeSelectButtonHeader;
