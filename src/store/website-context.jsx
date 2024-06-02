831d6ec6237b39e6802557feeec8f5a5a38dcc33
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-02 17:57:24 +0530

    fix: CORS error sorted, implemented google login and logout

--- src/store/website-context.jsx
+++ src/store/website-context.jsx
@@ -12,9 +12,15 @@ export default function WebContextProvider({ children }) {
     const [user, setUser] = useState(null);
 
     const glogout = () => {
+        const refresh_token = JSON.parse(localStorage.getItem("userData")).tokens.refresh
+        const access_token = JSON.parse(localStorage.getItem("userData")).tokens.access
         googleLogout();
         axios.post('https://conquest-api.bits-dvm.org/api/users/logout/', {
-            refresh_token: localStorage.getItem("userData")
+            refresh_token
+        }, {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
         })
         localStorage.removeItem("userData");
         setUser(null);
@@ -22,15 +28,15 @@ export default function WebContextProvider({ children }) {
 
     const glogin = useGoogleLogin({
         onSuccess: response => {
-            console.log(response)
+            // console.log(response)
             // Send access token
             axios.post('https://conquest-api.bits-dvm.org/api/users/login/google/', {
                 access_token: response.access_token
             }).then((res) => {
                 try {
-                    console.log(res)
-                    setUser(res.user_profile_obj)
-                    localStorage.setItem("userData", JSON.stringify(res))
+                    console.log(res.data)
+                    setUser(res.data.user_profile_obj)
+                    localStorage.setItem("userData", JSON.stringify(res.data))
                 }
                 catch (err) {
                     console.log(err)
