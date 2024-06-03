8cca4bd13400cfc718d6cb2ef04b6de078c0de05
Animeshdj <animeshstick.567@gmail.com>
2024-06-03 08:45:44 +0530

    feat: add startups page

---
+++ src/routes/Dashboard/Startups/Startups.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as styles from "./Startups.module.scss";
+
+const Startups = () => {
+  return <div className={styles.startups}>Startups</div>;
+};
+
+export default Startups;
