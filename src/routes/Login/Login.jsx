5c53d0cc79d647f5f6b8400d8c1916d7ed866b7a
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-03 21:25:31 +0530

    feat: removed unnecessary components

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,46 +1,35 @@
 import { useContext } from 'react';
 import { WebContext } from '../../store/website-context';
 
-import Countdown from '../../components/Countdown/Countdown';
-import GoogleSignOut from '../../components/Login/GoogleSignOut/GoogleSignOut';
-import Stepper from '../../components/Stepper/Stepper';
-import Stepper2 from '../../components/Login/Stepper2/Stepper2';
-import GoogleSignUpPage from '../../components/Login/GoogleSignUpPage/GoogleSignUpPage';
-import RegistrationForm from '../../components/Login/RegistrationForm/RegistrationForm';
+import { Divider } from 'antd';
 
-import logoImage from '../../assets/signUpLogo.png'
+import GoogleSignOut from '../../components/Login/GoogleSignOut/GoogleSignOut';
+import RegistrationForm from '../../components/Login/RegistrationForm/RegistrationForm';
+import GoogleSignIn from '../../components/GoogleSignIn/GoogleSignIn';
+
+import logoImage from '../../assets/loginPageLogo.png'
 
 import * as styles from './login.module.scss'
 
 export default function Login() {
     const { user } = useContext(WebContext);
-    let content = <GoogleSignUpPage />
-    if (user) {
-        content = <RegistrationForm />
-    }
     return (
         <main className={styles.container}>
             <GoogleSignOut />
             <img src={logoImage} alt="" className={styles.logo} />
             <div className={styles.loginContainer}>
                 <section className={styles.orangeSection}>
-                    <h1>Registration</h1>
-                    {/* <Stepper activeCircle={0} /> */}
-                    <div style={{
-                        width: '100%',
-                        height: '100%',
-                        display: 'flex',
-                        alignItems: 'center'
-                    }}>
-                        <Stepper2 />
-                    </div>
+
                 </section>
                 <section className={styles.whiteSection}>
-                    {content}
+                    <h1>Conquest Registration Portal</h1>
+                    <p>Elevate your startup's growth with our tailored program. Gain access to valuable resource pools, mentorship from CXOs, and fundraising opportunities.
+                        <br />
+                        <br />
+                        Tell us about your startup and yourself through our application form.
+                        We're excited to learn more about your venture and you.</p>
+                    <GoogleSignIn />
                 </section>
-            </div>
-            <div className={styles.countdownContainer}>
-                Registration Ends In: <Countdown dateString='June 7, 2024, 00:00:00' className={styles.countdown} />
             </div>
         </main>
     )
