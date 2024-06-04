8793da6a9c16b3e4b8bd72bfeba6f455c93d5fb4
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-04 17:37:38 +0530

    feat: persist state (may be buggy)

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,10 @@ import Login from "./routes/Login/Login";
 import LandingPage from "./routes/Landing/LandingPage";
 import ErrorPage from "./routes/ErrorPage/ErrorPage";
 
-import WebContextProvider from "./store/website-context";
-
 // importing and initializing react ga
 import ReactGA from "react-ga4";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { WebContext } from "./store/website-context";
 import Dashboard from "./routes/Dashboard/Dashboard";
 import Home from "./routes/Dashboard/Home/Home";
 import Meetings from "./routes/Dashboard/Meetings/Meetings";
@@ -18,6 +17,8 @@ ReactGA.initialize("G-ETE2M81K4Z"); // might have to put this in environment var
 
 function App() {
   // tracking page views
+  const { setUser, getUserData } = useContext(WebContext);
+  const userData = getUserData()
   // const location = useLocation();
   useEffect(() => {
     console.log(window.location.pathname + window.location.search); // for debugging
@@ -26,25 +27,27 @@ function App() {
       page: location.pathname + location.search,
       title: "Custom Title",
     });
+
+    if (userData) {
+      setUser(userData)
+    }
   }, []);
 
   return (
-    <WebContextProvider>
-      <Routes>
-        <Route
-          path="/"
-          element={<LandingPage />}
-          errorElement={<ErrorPage />}
-        />
-        <Route path="login" element={<Login />} />
-        <Route element={<RequireAuth />}>
-          <Route path="dashboard" element={<Dashboard />}>
-            <Route index element={<Home />}></Route>
-            <Route path="meetings" element={<Meetings />}></Route>
-          </Route>
+    <Routes>
+      <Route
+        path="/"
+        element={<LandingPage />}
+        errorElement={<ErrorPage />}
+      />
+      <Route path="login" element={<Login />} />
+      <Route element={<RequireAuth />}>
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route index element={<Home />}></Route>
+          <Route path="meetings" element={<Meetings />}></Route>
         </Route>
-      </Routes>
-    </WebContextProvider>
+      </Route>
+    </Routes>
   );
 }
 
