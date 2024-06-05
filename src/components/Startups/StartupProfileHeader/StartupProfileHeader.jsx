c0d261aa563d2de63cfab126c78ed13d88623923
Samyak Jain <jainsamyak1203@gmail.com>
2024-06-05 14:25:40 +0530

    feat: added header

--- src/components/Startups/StartupProfileHeader/StartupProfileHeader.jsx
+++ src/components/Startups/StartupProfileHeader/StartupProfileHeader.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import * as styles from "./StartupProfileHeader.module.scss";
 
-export default function StartupProfileHeader() {
+export default function StartupProfileHeader({ img, name, desc, location }) {
     return (
         <>
-           <div className={styles.logoContainer}></div>
+            <div className={styles.container}>
+                <div className={styles.contentContainer}>
+                    <div className={styles.logoContainer}>
+                        <img src={img} alt="" />
+                    </div>
+
+                    <div className={styles.headerContainer}>
+                        <div className={styles.name}>{name}</div>
+                        <div className={styles.desc}>{desc}</div>
+                        <div><span className={styles.location}>Location HQ : </span>{location}</div>
+                    </div>
+                </div>
+                <div className={styles.divider}></div>
+                <div className={styles.contactContainer}></div>
+            </div>
         </>
     );
 }
--- src/routes/Dashboard/StartupProfile/StartupProfile.jsx
+++ src/routes/Dashboard/StartupProfile/StartupProfile.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Button } from "antd";
 import * as styles from "./StartupProfile.module.scss";
 import StartupProfileHeader from "../../../components/Startups/StartupProfileHeader/StartupProfileHeader";
+import logo from "../../../assets/images/Dashboard/demoAvatar.jpeg";
 
 const dummyData = {
-
+    img: logo,
+    name: "Very long startup name",
+    desc: "Lorem ipsum dolor sit amet consectetur. Habitant accumsan feugiat quisque placerat porttitor.",
+    location: "Pilani, Rajasthan",
 }
 
 const StartupProfile = () => {
     return (
         <>
-            <StartupProfileHeader />
+            <StartupProfileHeader {...dummyData}/>
         </>
     );
 };
