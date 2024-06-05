f76f803f9bc6da0064bbfe34e0063297ef90114c
Animeshdj <animeshstick.567@gmail.com>
2024-06-05 15:40:18 +0530

    feat: add basic search

--- src/routes/Dashboard/Startups/Startups.jsx
+++ src/routes/Dashboard/Startups/Startups.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Input, Button } from "antd";
+import { Input, Dropdown } from "antd";
 import * as styles from "./Startups.module.scss";
 import StartupCard from "../../../components/Startups/StartupCard/StartupCard";
 
@@ -36,6 +36,21 @@ const exampleData = [
   },
 ];
 
+const FilterSVG = () => {
+  <svg
+    width="31"
+    height="18"
+    viewBox="0 0 31 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M28.5781 4.17188H2.42188C2.03648 4.17188 1.66687 4.01878 1.39436 3.74626C1.12185 3.47375 0.96875 3.10414 0.96875 2.71875C0.96875 2.33336 1.12185 1.96375 1.39436 1.69124C1.66687 1.41872 2.03648 1.26563 2.42188 1.26562H28.5781C28.9635 1.26562 29.3331 1.41872 29.6056 1.69124C29.8782 1.96375 30.0312 2.33336 30.0312 2.71875C30.0312 3.10414 29.8782 3.47375 29.6056 3.74626C29.3331 4.01878 28.9635 4.17188 28.5781 4.17188ZM23.7344 10.9531H7.26562C6.88023 10.9531 6.51062 10.8 6.23811 10.5275C5.9656 10.255 5.8125 9.88539 5.8125 9.5C5.8125 9.11461 5.9656 8.745 6.23811 8.47249C6.51062 8.19997 6.88023 8.04688 7.26562 8.04688H23.7344C24.1198 8.04688 24.4894 8.19997 24.7619 8.47249C25.0344 8.745 25.1875 9.11461 25.1875 9.5C25.1875 9.88539 25.0344 10.255 24.7619 10.5275C24.4894 10.8 24.1198 10.9531 23.7344 10.9531ZM17.9219 17.7344H13.0781C12.6927 17.7344 12.3231 17.5813 12.0506 17.3088C11.7781 17.0363 11.625 16.6666 11.625 16.2812C11.625 15.8959 11.7781 15.5262 12.0506 15.2537C12.3231 14.9812 12.6927 14.8281 13.0781 14.8281H17.9219C18.3073 14.8281 18.6769 14.9812 18.9494 15.2537C19.2219 15.5262 19.375 15.8959 19.375 16.2812C19.375 16.6666 19.2219 17.0363 18.9494 17.3088C18.6769 17.5813 18.3073 17.7344 17.9219 17.7344Z"
+      fill="black"
+    />
+  </svg>;
+};
+
 const Startups = () => {
   const [value, setValue] = useState("");
   return (
@@ -68,18 +83,18 @@ const Startups = () => {
             }
           ></Input>
         </div>
-        <div className={styles.filter}>
-          <Button>Filter</Button>
-        </div>
-        <div className={styles.tags}>
-          <Button>Tags</Button>
-        </div>
+        <div className={styles.filter}></div>
+        <div className={styles.tags}></div>
       </div>
       <h2>Showing results for {value ? value : ".."}</h2>
       <div className={styles.startupList}>
-        {exampleData.map((startup) => (
-          <StartupCard key={startup.id} {...startup} />
-        ))}
+        {exampleData
+          .filter((item) => {
+            if (item.name.includes(value)) return true;
+          })
+          .map((startup) => (
+            <StartupCard key={startup.id} {...startup} />
+          ))}
       </div>
     </div>
   );
