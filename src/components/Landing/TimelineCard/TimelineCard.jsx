101b07e94ce57dc6fcb505a615f77feb0070c0b3
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-06 23:46:37 +0530

    style: timeline responsive

--- src/components/Landing/TimelineCard/TimelineCard.jsx
+++ src/components/Landing/TimelineCard/TimelineCard.jsx
@@ -1,16 +1,35 @@
 import * as styles from "./TimelineCard.module.scss";
 
-const TimelineCard = ({ srno, date, title, para, srcolor }) => {
+const TimelineCard = ({ srno, date, title, para, srcolor, index = 0 }) => {
+  const srStyles = {
+    color: srcolor === "orange" ? "#FFE0D3" : "#F4F4F4",
+  };
+
+  const leftPartStyles = {
+    display: "flex",
+    flexDirection: "column",
+    // alignItems: index % 2 === 0 ? "flex-start" : "flex-end",
+    alignSelf: index % 2 === 0 ? "flex-start" : "flex-end",
+    width: "min-content",
+  }
+
+  if (index % 2 === 0) {
+    srStyles.left = "0";
+  }
+  else {
+    srStyles.right = "0";
+  }
+
   return (
     <div className={styles.Timelinecard}>
       <div
         className={styles.srno}
-        style={{ color: srcolor == "orange" ? "#FFE0D3" : "#F4F4F4" }}
+        style={srStyles}
       >
         {srno}
       </div>
       <div className={styles.cardContent}>
-        <div className={styles.leftPart}>
+        <div className={styles.leftPart} style={leftPartStyles}>
           <p className={styles.date}>{date}</p>
           <p className={styles.title}>{title}</p>
         </div>
