8e5ad30b6221c6e08232e876ea64a9e501229182
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 02:43:35 +0530

    feat: complete jsx of bookslots

--- src/components/Dashboard/Meetings/BootSlots/BookSlots.jsx
+++ src/components/Dashboard/Meetings/BootSlots/BookSlots.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import styles from "./BookSlots.module.scss";
+import SlotInputField from "../SelectSlots/SlotInputField/SlotInputField";
 
 function BookSlots() {
   return (
-    <div>BookSlots</div>
-  )
+    <div className={styles.container}>
+      <h2>BookSlots</h2>
+      <SlotInputField></SlotInputField>
+      <SlotInputField></SlotInputField>
+      <SlotInputField></SlotInputField>
+      <button>Confirm</button>
+    </div>
+  );
 }
 
-export default BookSlots
+export default BookSlots;
