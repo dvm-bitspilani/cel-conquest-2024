d1f2eb388e88c77366f05a2df56995763cf28b7d
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-08 00:26:48 +0530

    style: input boxes spacing and positioning

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -23,7 +23,7 @@ export default function TextInput({ name, heading, changeFn, blurFn, value, erro
                         onBlur={blurFn}
                         value={value}
                         status={error ? 'error' : ''}
-                        style={error ? { backgroundColor: '#FFE6E6', width: '100%' } : { width: '100%' }}
+                        style={error ? { backgroundColor: '#FFE6E6', width: '100%', height: '100%' } : { width: '100%', height: '100%' }}
                         {...props}
                     />
                 </ConfigProvider>
