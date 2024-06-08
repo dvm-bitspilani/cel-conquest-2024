de700e9742a3a428f0fac4312a24ca11f9a1544e
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 16:30:54 +0530

    fix: svg error stroke-linecap

--- src/components/Dashboard/Meetings/MeetingDetails/MeetingDetails.jsx
+++ src/components/Dashboard/Meetings/MeetingDetails/MeetingDetails.jsx
@@ -49,35 +49,35 @@ const MeetingDetails = () => {
               d="M18 8H19C20.0609 8 21.0783 8.42143 21.8284 9.17157C22.5786 9.92172 23 10.9391 23 12C23 13.0609 22.5786 14.0783 21.8284 14.8284C21.0783 15.5786 20.0609 16 19 16H18"
               stroke="#A0A0A0"
               strokeWidth="2"
-              stroke-linecap="round"
+              strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M2 8H18V17C18 18.0609 17.5786 19.0783 16.8284 19.8284C16.0783 20.5786 15.0609 21 14 21H6C4.93913 21 3.92172 20.5786 3.17157 19.8284C2.42143 19.0783 2 18.0609 2 17V8Z"
               stroke="#A0A0A0"
               strokeWidth="2"
-              stroke-linecap="round"
+              strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M6 1V4"
               stroke="#A0A0A0"
               strokeWidth="2"
-              stroke-linecap="round"
+              strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M10 1V4"
               stroke="#A0A0A0"
               strokeWidth="2"
-              stroke-linecap="round"
+              strokeLinecap="round"
               strokeLinejoin="round"
             />
             <path
               d="M14 1V4"
               stroke="#A0A0A0"
               strokeWidth="2"
-              stroke-linecap="round"
+              strokeLinecap="round"
               strokeLinejoin="round"
             />
           </svg>
@@ -113,14 +113,14 @@ const MeetingDetails = () => {
                   d="M5.83325 14.1666L14.1666 5.83325"
                   stroke="#138CFD"
                   strokeWidth="1.5"
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                   strokeLinejoin="round"
                 />
                 <path
                   d="M5.83325 5.83325H14.1666V14.1666"
                   stroke="#138CFD"
                   strokeWidth="1.5"
-                  stroke-linecap="round"
+                  strokeLinecap"round"
                   strokeLinejoin="round"
                 />
               </svg>
--- src/components/Dashboard/Meetings/SlotTimingSelector/TimeSelectButton/TimeSelectButton.jsx
+++ src/components/Dashboard/Meetings/SlotTimingSelector/TimeSelectButton/TimeSelectButton.jsx
@@ -17,19 +17,19 @@ function TimeSelectButton({ time, changeTime, dateTime}) {
           d="M10.752 18.3332C15.3544 18.3332 19.0854 14.6022 19.0854 9.99984C19.0854 5.39746 15.3544 1.6665 10.752 1.6665C6.14966 1.6665 2.4187 5.39746 2.4187 9.99984C2.4187 14.6022 6.14966 18.3332 10.752 18.3332Z"
           stroke="#A0A0A0"
           strokeWidth="1.5"
-          stroke-linecap="round"
+          strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
           d="M15.252 7.5L9.06445 12.5L6.25195 10.2273"
           stroke="#A0A0A0"
           strokeWidth="1.5"
-          stroke-linecap="round"
+          strokeLinecap="round"
           strokeLinejoin="round"
         />
       </svg>):(<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M10.752 18.3337C15.3544 18.3337 19.0854 14.6027 19.0854 10.0003C19.0854 5.39795 15.3544 1.66699 10.752 1.66699C6.14966 1.66699 2.4187 5.39795 2.4187 10.0003C2.4187 14.6027 6.14966 18.3337 10.752 18.3337Z" fill="#FB723D"/>
-<path d="M15.252 7.5L9.06445 12.5L6.25195 10.2273" stroke="#FFECE5" strokeWidth="1.5" stroke-linecap="round" strokeLinejoin="round"/>
+<path d="M15.252 7.5L9.06445 12.5L6.25195 10.2273" stroke="#FFECE5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 </svg> )}
       <p>{time} AM</p>
     </button>
