7c73466aac53564712bd6d0de967e0e65fdeb741
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 18:22:24 +0530

    feat: create startup pov book slot component

---
+++ src/components/Dashboard/Meetings/BookSlots/BookSlotItem/BookSlotItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './BookSlotItem.module.scss'
+
+function BookSlotItem() {
+  return (
+    <div>BookSlotItem</div>
+  )
+}
+
+export default BookSlotItem
--- src/components/Dashboard/Meetings/BookSlots/BookSlots.jsx
+++ src/components/Dashboard/Meetings/BookSlots/BookSlots.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./BookSlots.module.scss";
 import SlotInputField from "../SelectSlots/SlotInputField/SlotInputField";
+import BookSlotItem from "./BookSlotItem/BookSlotItem";
 
 function BookSlots() {
   return (
     <div className={styles.container}>
       <h2>BookSlots</h2>
-      <SlotInputField></SlotInputField>
-      <SlotInputField></SlotInputField>
-      <SlotInputField></SlotInputField>
+      <BookSlotItem></BookSlotItem>
+      <BookSlotItem></BookSlotItem>
+      <BookSlotItem></BookSlotItem>
       <button>Confirm</button>
     </div>
   );
