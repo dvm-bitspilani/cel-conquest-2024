dde85fbc5c564b5f4eb955f11c867b717e4de494
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 18:02:35 +0530

    feat: date time login complete

--- src/components/Dashboard/Meetings/SelectSlots/SlotInputField/SlotInputField.jsx
+++ src/components/Dashboard/Meetings/SelectSlots/SlotInputField/SlotInputField.jsx
@@ -1,24 +1,41 @@
 import styles from "./SlotInputField.module.scss";
 
-const SlotInputField = ({ showHideSelectSlotTiming , id, dateTimeStart}) => {
+const SlotInputField = ({
+  showHideSelectSlotTiming,
+  id,
+  dateTimeStart,
+  dateTimeEnd,
+}) => {
+  const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dateObjEnd = new Date(dateTimeEnd);
   const dateObj = new Date(dateTimeStart);
-    const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
-        dateObj
-    );
-    const meetDate = dateObj.getDate();
+  const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
+    dateObj
+  );
+  console.log(dateTimeEnd===dateTimeStart)
+  const meetDate = dateObj.getDate();
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes();
+  const fullTime =
+    hours > 12
+      ? `${hours - 12}:${minutes} PM`
+      : hours === 0
+      ? `12:${minutes} AM`
+      : `${hours}:${minutes} AM`;
 
-    const hours = dateObj.getHours();
-    const minutes = dateObj.getMinutes();
-    const fullTime =
-        hours > 12
-            ? `${hours - 12}:${minutes} PM`
-            : hours === 0
-                ? `12:${minutes} AM`
-                : `${hours}:${minutes} AM`;
+  const hoursEnd = dateObjEnd.getHours();
+  const minutesEnd = dateObjEnd.getMinutes();
+  const fullTimeEnd =
+    hoursEnd > 12
+      ? `${hoursEnd - 12}:${minutesEnd} PM`
+      : hoursEnd === 0
+      ? `12:${minutesEnd} AM`
+      : `${hoursEnd}:${minutesEnd} AM`;
   return (
     <div className={styles.inputField}>
-      
-      <p>Slot {id} ({dateStart.getDate()} {dateStart.getDate()} ()) </p>
+      <p>
+        Slot {id} ({meetDate} {month} ({week[dateObj.getDay()]}), {fullTime} - {fullTimeEnd})
+      </p>
       <button
         className={styles.SelectButton}
         onClick={showHideSelectSlotTiming}
