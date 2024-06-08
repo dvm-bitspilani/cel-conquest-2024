ad43c19dd4b978dace97d2e3b33c6672ed805bbb
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 20:49:36 +0530

    feat: add connection list item component

---
+++ src/components/Connections/ConnectionListItem/ConnectionListItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ConnectionListItem.module.scss'
+
+function ConnectionListItem() {
+  return (
+    <div>ConnectionListItem</div>
+  )
+}
+
+export default ConnectionListItem
