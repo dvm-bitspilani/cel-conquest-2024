ad56269d42e44842548911c91e03e29057424f90
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 21:52:42 +0530

    feat: add getList function to Connections.jsx

--- src/routes/Dashboard/Connections/Connections.jsx
+++ src/routes/Dashboard/Connections/Connections.jsx
@@ -2,10 +2,52 @@ import React from "react";
 import styles from "./Connections.module.scss";
 import { useState } from "react";
 
-
 function Connections() {
   const [listTab, setListTab] = useState("pending");
-  const getList = () => {};
+
+  const getList = (listTab) => {
+    if (JSON.parse(localStorage.getItem("userData")).tokens) {
+      console.log("fetching data");
+      axios
+        .get(
+          `https://conquest-api.bits-dvm.org/api/meetings/meetings/${listTab}/`,
+          {
+            headers: {
+              Authorization: `Bearer ${
+                JSON.parse(localStorage.getItem("userData")).tokens.access
+              }`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+
+          const newArr = res.data.map((newItm) => {
+            return (
+              <MeetingItem
+                date={newItm.slot_start_time}
+                avatar={newItm.requested_logo}
+                mentorName={newItm.requested_name}
+                duration={45}
+                key={newItm.id}
+                // data={newItm}
+                handleClick={handleClick}
+                dataRef={dataRef}
+              />
+            );
+          });
+
+          setListItms(newArr);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log("error in fetching data");
+    }
+  };
+
+  
   return (
     <div className={styles.container}>
       <div className={styles.ButtonContainer}>
