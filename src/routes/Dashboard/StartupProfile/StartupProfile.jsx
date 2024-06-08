58a6cdf4dc9742c701e3baf877c18b3f48bb2627
Samyak Jain <jainsamyak1203@gmail.com>
2024-06-09 00:16:17 +0530

    feat: added new state and fixed import issues

--- src/routes/Dashboard/StartupProfile/StartupProfile.jsx
+++ src/routes/Dashboard/StartupProfile/StartupProfile.jsx
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as styles from "./StartupProfile.module.scss";
 import StartupProfileHeader from "../../../components/Startups/StartupProfileHeader/StartupProfileHeader";
+import StartupProfileContact from "../../../components/Startups/StartupProfileContact/StartupProfileContact";
+import ProfileButton from "../../../components/Startups/ProfileButton/ProfileButton";
+import About from "../../../components/Startups/About/About";
+import Team from "../../../components/Startups/Team/Team";
+import Details from "../../../components/Startups/Details/Details";
+import Pitch from "../../../components/Startups/Pitch/Pitch";
 import logo from "../../../assets/images/Dashboard/demoAvatar.jpeg";
 import axios from "axios";
 
-const dummyData = {
-    img: logo,
-    name: "Very long startup name",
-    desc: "Lorem ipsum dolor sit amet consectetur. Habitant accumsan feugiat quisque placerat porttitor.",
-    location: "Pilani, Rajasthan",
-}
+// const dummyData = {
+//     img: logo,
+//     name: "Very long startup name",
+//     desc: "Lorem ipsum dolor sit amet consectetur. Habitant accumsan feugiat quisque placerat porttitor.",
+//     location: "Pilani, Rajasthan",
+// }
 
 let startupId = 1
 
 const StartupProfile = () => {
+
+    const [startupProfile, setstartupProfile] = useState({});
+    const [selectedTopic, setSelectedTopic] = useState('about');
 
     useEffect(() => {
         if (JSON.parse(localStorage.getItem("userData")).tokens) {
@@ -30,7 +39,9 @@ const StartupProfile = () => {
                     }
                 )
                 .then(function (response) {
-                    console.log(response.data);
+                    // console.log(response.data);
+                    setstartupProfile(response.data);
+                    // console.log(startupProfile)
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -39,10 +50,12 @@ const StartupProfile = () => {
             console.log("error in fetching data");
         }
 
-    }, [JSON.parse(localStorage.getItem("userData")).tokens.access]);
+    }, [startupId]);
+    
+    console.log(startupProfile)
     return (
         <>
-            <StartupProfileHeader {...dummyData} />
+            <StartupProfileHeader img={startupProfile.profile_logo} name={startupProfile.startup_name} desc={startupProfile.description} location={startupProfile.location_hq} />
         </>
     );
 };
