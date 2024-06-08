72e19650a17cbf8c81d177537089b86fd76c4e13
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 21:40:24 +0530

    feat: add active state to connections

--- src/routes/Dashboard/Connections/Connections.jsx
+++ src/routes/Dashboard/Connections/Connections.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import styles from "./Connections.module.scss";
 
 function Connections() {
-  return <div className={styles.container}>
-    <div className={styles.ButtonContainer}>
-      <button className={styles.PendingButton}>Pending</button>
-      <button className={styles.ConnectionsButton}>My Connections</button>
+  return (
+    <div className={styles.container}>
+      <div className={styles.ButtonContainer}>
+        <div
+          onClick={() => {
+            setListTab("pending");
+            getMeetingList("pending");
+          }}
+          className={`${styles.meetingsListOptions} ${
+            listTab === "pending" ? styles.active : null
+          }`}
+        >
+          Pending
+        </div>
+        <button className={`${styles.ConnectionsButton}`}>
+          My Connections
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Connections;
