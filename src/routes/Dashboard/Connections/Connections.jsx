cae5f2614f696a3a7d722b9f3106312f55a963a7
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-08 21:29:37 +0530

    feat: add my connections and pending requestions button

--- src/routes/Dashboard/Connections/Connections.jsx
+++ src/routes/Dashboard/Connections/Connections.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./Connections.module.scss";
 
 function Connections() {
-  return <div>Connections</div>;
+  return <div className={styles.container}>
+    <div className={styles.ButtonContainer}>
+      <button className={styles.PendingButton}>Pending</button>
+      <button className={styles.ConnectionsButton}>My Connections</button>
+
+    </div>
+  </div>;
 }
 
 export default Connections;
