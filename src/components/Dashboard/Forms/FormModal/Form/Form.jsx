ffcc28eb6d896ebfd6654d79452a0bd485d83533
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-09 21:30:07 +0530

    feat: text input component

--- src/components/Dashboard/Forms/FormModal/Form/Form.jsx
+++ src/components/Dashboard/Forms/FormModal/Form/Form.jsx
@@ -1,9 +1,14 @@
+import TextInput from '../Inputs/Text/TextInput'
+
 import styles from './form.module.scss'
 
 export default function Form({ data }) {
     return (
-        <>
-            <button type='submit' className={styles.submit}>Submit</button>
-        </>
+        <main className={styles.form}>
+            <div className={styles.wrapper}>
+                <TextInput />
+                <button type='submit' className={styles.submit}>Submit</button>
+            </div>
+        </main>
     )
 }
--- src/components/Dashboard/Forms/FormModal/Inputs/Text/TextInput.jsx
+++ src/components/Dashboard/Forms/FormModal/Inputs/Text/TextInput.jsx
@@ -2,6 +2,18 @@ import styles from './textinput.module.scss'
 
 export default function TextInput() {
     return (
-        <h1>Text Input</h1>
+        <div className={styles.inputGrp}>
+            <label
+                htmlFor='question'
+                className={styles.label}>
+                Question
+            </label>
+            <input
+                type='text'
+                id='question'
+                name='question'
+                className={styles.input}
+            />
+        </div>
     )
 }
