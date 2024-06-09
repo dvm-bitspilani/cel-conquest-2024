796af43d81836e1f51b8191230444eb200950ccb
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-09 16:27:21 +0530

    created forms page components

---
+++ src/components/Dashboard/Forms/FormPill2/Item/Item.jsx
@@ -0,0 +1,9 @@
+import styles from './item.module.scss'
+
+export default function FormPillItem2() {
+    return (
+        <div className={styles.pillBox}>
+            <h1>Item</h1>
+        </div>
+    )
+}
