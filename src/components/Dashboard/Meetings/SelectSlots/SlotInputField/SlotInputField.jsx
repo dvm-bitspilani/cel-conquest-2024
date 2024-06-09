2f25c15d8275dda1771d7c5f4bf007e35383b425
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-09 16:08:36 +0530

    change delete buttond design on phone screen

--- src/components/Dashboard/Meetings/SelectSlots/SlotInputField/SlotInputField.jsx
+++ src/components/Dashboard/Meetings/SelectSlots/SlotInputField/SlotInputField.jsx
@@ -51,7 +51,32 @@ const SlotInputField = ({
           return deleteSlot(id);
         }}
       >
-        Delete
+        {window.innerWidth > 600 ? (
+          "Delete"
+        ) : (
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18 6L6 18"
+              stroke="white"
+              stroke-width="3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M6 6L18 18"
+              stroke="white"
+              stroke-width="3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        )}
       </button>
     </div>
   );
