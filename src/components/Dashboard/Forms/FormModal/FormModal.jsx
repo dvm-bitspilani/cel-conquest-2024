0314f76f545dd44274c4643d98b08387503c63ff
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-09 21:09:26 +0530

    fix: syntax error

--- src/components/Dashboard/Forms/FormModal/FormModal.jsx
+++ src/components/Dashboard/Forms/FormModal/FormModal.jsx
@@ -29,8 +29,8 @@ const FormModal = forwardRef(function ({ data }, ref) {
             className={styles.modal}
         >
             <svg onClick={closeModal} className={styles.cross} viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M24.75 10.4502L8.25 26.9502" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M8.25 10.4502L24.75 26.9502" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M24.75 10.4502L8.25 26.9502" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M8.25 10.4502L24.75 26.9502" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
             <h2 className={styles.heading}>{data.form_name}</h2>
             <main className={styles.questionsContainer}></main>
