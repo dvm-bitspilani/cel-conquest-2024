76a56db42a94b14bf5c90f32abc584db5eb83604
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-09 11:23:13 +0530

    feat: BookSlots component startup pov

--- src/components/Dashboard/Meetings/BookSlots/BookSlots.jsx
+++ src/components/Dashboard/Meetings/BookSlots/BookSlots.jsx
@@ -3,13 +3,51 @@ import styles from "./BookSlots.module.scss";
 import BookSlotItem from "./BookSlotItem/BookSlotItem";
 
 function BookSlots() {
+  const [slotList, setSlotList] = useState([]);
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("userData")).tokens) {
+      axios
+        .get("https://conquest-api.bits-dvm.org/api/meetings/slots/", {
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("userData")).tokens.access
+            }`,
+          },
+        })
+        .then((res) => {
+          const newArr = res.data.map((newItm, index) => {
+            return (
+              <SlotInputField
+                slotno={index + 1}
+                key={newItm.id}
+                id={newItm.id}
+                showHideSelectSlotTiming={showHideSelectSlotTiming}
+                dateTimeStart={newItm.start_time}
+                dateTimeEnd={newItm.end_time}
+                deleteSlot={deleteSlot}
+              ></SlotInputField>
+            );
+          });
+          setSlotList(newArr);
+          console.log(res.data);
+          setSlotData(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log("error in fetching data");
+    }
+  }, [JSON.parse(localStorage.getItem("userData")).tokens.access]);
   return (
-    <div className={styles.container}>
-      <h2>BookSlots</h2>
-      <BookSlotItem></BookSlotItem>
-      <BookSlotItem></BookSlotItem>
-      <BookSlotItem></BookSlotItem>
-      <button>Confirm</button>
+    <div className={styles.SelectSlots}>
+      <div className={styles.slotListWrapper}>
+        <h3>Book Slots</h3>
+        {slotList}
+        <div className={styles.addDone}>
+          <button onClick={showHideSelectSlotTiming}>Confirm</button>
+        </div>
+      </div>
     </div>
   );
 }
