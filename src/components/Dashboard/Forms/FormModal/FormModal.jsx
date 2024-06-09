db0bd7304bd16a026a045e07197c7746b84c6aad
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-09 19:39:41 +0530

    style: styled the form modal

--- src/components/Dashboard/Forms/FormModal/FormModal.jsx
+++ src/components/Dashboard/Forms/FormModal/FormModal.jsx
@@ -15,8 +15,17 @@ const FormModal = forwardRef(function ({ }, ref) {
         )
     })
 
+    function closeModal() {
+        dialog.current.close()
+    }
+
     return (
         <dialog ref={dialog} className={styles.modal}>
+            <svg onClick={closeModal} className={styles.cross} viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.75 10.4502L8.25 26.9502" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M8.25 10.4502L24.75 26.9502" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+
             <h1>Test</h1>
         </dialog>
     )
