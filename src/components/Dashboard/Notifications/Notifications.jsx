f954e0694baab9202f6dcc9e0d893f9f4bc7acef
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-10 11:41:18 +0530

    feat: add notifications

---
+++ src/components/Dashboard/Notifications/Notifications.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Notifications.module.scss'
+
+function Notifications() {
+  return (
+    <div>Notifications</div>
+  )
+}
+
+export default Notifications
