39d4e9b49f314257d5608e89d2fff41492805008
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-10 22:15:10 +0530

    feat: added back api in home page form

--- src/components/Dashboard/Forms/FormPillList/FormPillList.jsx
+++ src/components/Dashboard/Forms/FormPillList/FormPillList.jsx
@@ -84,20 +84,20 @@ export default function FormPillList() {
     const formModal = useRef(null)
 
     function formOpenHandler(formId) {
-        // axios.get(`https://conquest-api.bits-dvm.org/api/forms/${formId}/questions/`, {
-        //     headers: {
-        //         Authorization: `Bearer ${JSON.parse(localStorage.getItem('userData')).tokens.access}`
-        //     }
-        // })
-        //     .then(res => {
-        //         setModalData(res.data)
-        //         formModal.current.openForm()
-        //     })
-        //     .catch(err => {
-        //         console.log(err)
-        //     })
-        setModalData(DUMMY_QUESTIONS)
-        formModal.current.openForm()
+        axios.get(`https://conquest-api.bits-dvm.org/api/forms/${formId}/questions/`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem('userData')).tokens.access}`
+            }
+        })
+            .then(res => {
+                setModalData(res.data)
+                formModal.current.openForm()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        // setModalData(DUMMY_QUESTIONS)
+        // formModal.current.openForm()
     }
 
     useEffect(() => {
