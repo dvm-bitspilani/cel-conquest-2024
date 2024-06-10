171d5853e36578091fd996dc33198da6a693e84a
chaitanya-rahul-jain <152156734+chaitanya-rahul-jain@users.noreply.github.com>
2024-06-10 12:05:33 +0530

    feat: notifications layout and styling

--- src/components/Dashboard/Notifications/Notifications.jsx
+++ src/components/Dashboard/Notifications/Notifications.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import styles from './Notifications.module.scss'
+import React from "react";
+import styles from "./Notifications.module.scss";
+
+const cross = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18 6L6 18"
+      stroke="black"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 6L18 18"
+      stroke="black"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
 
 function Notifications() {
   return (
-    <div>Notifications</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.notifHeader}>
+        <h3>Notifications</h3>
+        {cross}
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
-export default Notifications
+export default Notifications;
