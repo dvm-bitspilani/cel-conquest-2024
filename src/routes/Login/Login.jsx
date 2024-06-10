ed3e3c08cc72b56517669249086c0a74492912ab
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-10 20:03:47 +0530

    feat: redirect logged in user onload

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -38,7 +38,7 @@ export default function Login() {
 
     useEffect(() => {
         if (user) {
-            // navigate('/dashboard')
+            navigate('/dashboard')
         }
     }, [user])
 
