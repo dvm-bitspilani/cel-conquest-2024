680e9e80d8715f261f7f74a490b190b76e1d48a7
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-10 20:00:02 +0530

    feat: added reorder list component

--- src/components/Dashboard/Forms/FormModal/Inputs/Preference/Preference.jsx
+++ src/components/Dashboard/Forms/FormModal/Inputs/Preference/Preference.jsx
@@ -1,7 +1,22 @@
-export default function Preference({ options }) {
+import { Reorder } from "framer-motion"
+import { useEffect, useState } from "react"
+
+export default function Preference({ options, name, manualValue, heading }) {
+    const [items, setItems] = useState(["First", "Second", "Third", "Fourth"])
+
+    useEffect(() => {
+        console.log(items)
+    }, [items])
+
     return (
         <div>
-            <p>Preference</p>
+            <Reorder.Group values={items} onReorder={setItems}>
+                {items.map(item => (
+                    <Reorder.Item key={item} value={item}>
+                        {item}
+                    </Reorder.Item>
+                ))}
+            </Reorder.Group>
         </div>
     )
 }
