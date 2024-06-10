e87922c49abd606a4ec314529797d08341e4b409
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-10 21:51:55 +0530

    feat: completed logic for preference type component

--- src/components/Dashboard/Forms/FormModal/Inputs/Preference/Preference.jsx
+++ src/components/Dashboard/Forms/FormModal/Inputs/Preference/Preference.jsx
@@ -4,10 +4,20 @@ import { useEffect, useState } from "react"
 import styles from './preference.module.scss'
 
 export default function Preference({ options, name, manualValue, heading }) {
-    const [items, setItems] = useState(["First", "Second", "Third", "Fourth"])
+    const [items, setItems] = useState(options)
 
     useEffect(() => {
-        console.log(items)
+        // console.log(items)
+        const answerKey = items.map(answer => {
+            return (
+                {
+                    id: answer.id,
+                    position: items.indexOf(answer)
+                }
+            )
+        })
+        // console.log(answerKey)
+        manualValue(name, answerKey)
     }, [items])
 
     return (
@@ -15,14 +25,14 @@ export default function Preference({ options, name, manualValue, heading }) {
             <p className={styles.label}>{heading}</p>
             <Reorder.Group values={items} onReorder={setItems}>
                 {items.map(item => (
-                    <Reorder.Item key={item} value={item}>
+                    <Reorder.Item key={item.preference_name} value={item}>
                         <div className={styles.option}>
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M3 12H21" stroke="#898989" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                 <path d="M3 6H21" stroke="#898989" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                 <path d="M3 18H21" stroke="#898989" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
-                            {item}
+                            {item.preference_name}
                         </div>
                     </Reorder.Item>
                 ))}
