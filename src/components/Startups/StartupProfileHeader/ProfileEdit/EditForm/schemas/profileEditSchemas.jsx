4aab7e9b6bc110165a852455f07380761c6b391d
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-17 22:28:59 +0530

    feat: schemas for password and confirm password

--- src/components/Startups/StartupProfileHeader/ProfileEdit/EditForm/schemas/profileEditSchemas.jsx
+++ src/components/Startups/StartupProfileHeader/ProfileEdit/EditForm/schemas/profileEditSchemas.jsx
@@ -2,4 +2,6 @@ import * as Yup from 'yup';
 
 export const profileSchemas = Yup.object({
     email: Yup.string().email('Invalid email address'),
+    password: Yup.string().min(8, 'Password is too short - should be 8 chars minimum.'),
+    confirm_password: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
 })
