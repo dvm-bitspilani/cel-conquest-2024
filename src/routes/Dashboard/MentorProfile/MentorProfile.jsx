7bd2ff7a660948d64f0fa31c0adebefe79acb811
Manas Choudhary <151459346+ManasChoudhary-01@users.noreply.github.com>
2024-06-19 21:16:12 +0530

    Uploading Mentor Profile route.

---
+++ src/routes/Dashboard/MentorProfile/MentorProfile.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import MentorProfileHeader from "../../../components/Mentors/MentorProfileHeader/MentorProfileHeader";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const MentorProfile = () => {
+  const { id } = useParams();
+  const [startupProfile, setstartupProfile] = useState({});
+  // const [userProfile, setuserProfile] = useState({});
+  // const [team, setTeam] = useState({});
+
+  useEffect(() => {
+    if (id) {
+      if (JSON.parse(localStorage.getItem("userData")).tokens) {
+        axios
+          .get(
+            `https://conquest-api.bits-dvm.org/api/users/profile_detail/?id=${id}`,
+            {
+              headers: {
+                Authorization: `Bearer ${
+                  JSON.parse(localStorage.getItem("userData")).tokens.access
+                }`,
+              },
+              params: {
+                id: id,
+              },
+            }
+          )
+          .then(function (response) {
+            setstartupProfile(response.data);
+            // setuserProfile(response.data.user_profile);
+            // console.log(response.data.user_profile);
+            // setTeam(response.data.team_member);
+            console.log(startupProfile);
+          })
+          .catch(function (error) {
+            console.log(error);
+            console.log(id);
+          });
+      } else {
+        console.log("error in fetching data");
+      }
+    } else {
+      setstartupProfile(
+        JSON.parse(localStorage.getItem("userData")).user_profile_obj
+      );
+    }
+  }, [JSON.parse(localStorage.getItem("userData")).tokens.access, id]);
+
+  return (
+    <>
+      <MentorProfileHeader
+        img={startupProfile.profile_logo}
+        name={startupProfile.name}
+        desg={startupProfile.designation}
+        desc={startupProfile.description}
+        location={startupProfile.location}
+        sector={startupProfile.sector_of_expertise}
+        domain={startupProfile.domain_of_expertise}
+        resume={startupProfile}
+        twitter={startupProfile.twitter}
+        linkedin={startupProfile.linkedin}
+        schedulebtn={startupProfile.role}
+        startupid={id}
+      />
+    </>
+  );
+};
+
+export default MentorProfile;
