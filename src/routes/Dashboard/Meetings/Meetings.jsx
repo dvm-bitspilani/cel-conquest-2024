df25ef40c4a643d41275868bde28c945977f0f5c
AtharvAgarwal20 <atharv20agarwal@gmail.com>
2024-06-20 18:06:40 +0530

    style: highlight global events

--- src/routes/Dashboard/Meetings/Meetings.jsx
+++ src/routes/Dashboard/Meetings/Meetings.jsx
@@ -15,7 +15,7 @@ const Meetings = () => {
   const [requestSent, setRequestSent] = useState(false);
   const isStartup =
     JSON.parse(localStorage.getItem("userData")).user_profile_obj.role ===
-    "Startup"
+      "Startup"
       ? true
       : false;
 
@@ -54,9 +54,8 @@ const Meetings = () => {
           `https://conquest-api.bits-dvm.org/api/meetings/meetings/${listTab}/`,
           {
             headers: {
-              Authorization: `Bearer ${
-                JSON.parse(localStorage.getItem("userData")).tokens.access
-              }`,
+              Authorization: `Bearer ${JSON.parse(localStorage.getItem("userData")).tokens.access
+                }`,
             },
           }
         )
@@ -69,15 +68,15 @@ const Meetings = () => {
                 date={newItm.slot_start_time}
                 avatar={
                   newItm.requested_name ===
-                  JSON.parse(localStorage.getItem("userData")).user_profile_obj
-                    .name
+                    JSON.parse(localStorage.getItem("userData")).user_profile_obj
+                      .name
                     ? newItm.requester_logo
                     : newItm.requested_logo
                 }
                 mentorName={
                   newItm.requested_name ===
-                  JSON.parse(localStorage.getItem("userData")).user_profile_obj
-                    .name
+                    JSON.parse(localStorage.getItem("userData")).user_profile_obj
+                      .name
                     ? newItm.requester_name
                     : newItm.requested_name
                 }
@@ -111,9 +110,8 @@ const Meetings = () => {
           `https://conquest-api.bits-dvm.org/api/meetings/meetings/upcoming/`,
           {
             headers: {
-              Authorization: `Bearer ${
-                JSON.parse(localStorage.getItem("userData")).tokens.access
-              }`,
+              Authorization: `Bearer ${JSON.parse(localStorage.getItem("userData")).tokens.access
+                }`,
             },
           }
         )
@@ -126,15 +124,15 @@ const Meetings = () => {
                 date={newItm.slot_start_time}
                 avatar={
                   newItm.requested_name ===
-                  JSON.parse(localStorage.getItem("userData")).user_profile_obj
-                    .name
+                    JSON.parse(localStorage.getItem("userData")).user_profile_obj
+                      .name
                     ? newItm.requester_logo
                     : newItm.requested_logo
                 }
                 mentorName={
                   newItm.requested_name ===
-                  JSON.parse(localStorage.getItem("userData")).user_profile_obj
-                    .name
+                    JSON.parse(localStorage.getItem("userData")).user_profile_obj
+                      .name
                     ? newItm.requester_name
                     : newItm.requested_name
                 }
@@ -143,6 +141,7 @@ const Meetings = () => {
                 data={newItm}
                 handleClick={handleClick}
                 dataRef={dataRef}
+                isGlobal={false}
                 type={listTab === "pending" ? "accept-decline" : "join"}
               />
             );
@@ -153,8 +152,8 @@ const Meetings = () => {
                 date={newItm.start_time}
                 avatar={
                   newItm.requested_name ===
-                  JSON.parse(localStorage.getItem("userData")).user_profile_obj
-                    .name
+                    JSON.parse(localStorage.getItem("userData")).user_profile_obj
+                      .name
                     ? newItm.requester_logo
                     : newItm.requested_logo
                 }
@@ -163,6 +162,7 @@ const Meetings = () => {
                 key={newItm.id}
                 data={newItm}
                 handleClick={handleClick}
+                isGlobal={true}
                 dataRef={dataRef}
               />
             );
@@ -215,9 +215,8 @@ const Meetings = () => {
                 setListTab("upcoming");
                 getMeetingList("upcoming");
               }}
-              className={`${styles.meetingsListOptions} ${
-                listTab === "upcoming" ? styles.active : null
-              }`}
+              className={`${styles.meetingsListOptions} ${listTab === "upcoming" ? styles.active : null
+                }`}
             >
               Upcoming
             </div>
@@ -226,9 +225,8 @@ const Meetings = () => {
                 setListTab("pending");
                 getMeetingList("pending");
               }}
-              className={`${styles.meetingsListOptions} ${
-                listTab === "pending" ? styles.active : null
-              }`}
+              className={`${styles.meetingsListOptions} ${listTab === "pending" ? styles.active : null
+                }`}
             >
               Pending
             </div>
@@ -237,9 +235,8 @@ const Meetings = () => {
                 setListTab("past");
                 getMeetingList("past");
               }}
-              className={`${styles.meetingsListOptions} ${
-                listTab === "past" ? styles.active : null
-              }`}
+              className={`${styles.meetingsListOptions} ${listTab === "past" ? styles.active : null
+                }`}
             >
               Past
             </div>
@@ -266,9 +263,8 @@ const Meetings = () => {
             ></SelectSlots>
           ) : null}
           <div
-            className={`${styles.meetingsDetails} ${
-              selectSlots ? styles.blur : null
-            }`}
+            className={`${styles.meetingsDetails} ${selectSlots ? styles.blur : null
+              }`}
           >
             {data !== null ? (
               <MeetingDetails myData={data}></MeetingDetails>
