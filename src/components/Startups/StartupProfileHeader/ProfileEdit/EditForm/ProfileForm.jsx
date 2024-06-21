b43d64bd64b1f1a737d6c3f9a00e3714503ed1e8
Animeshdj <animeshstick.567@gmail.com>
2024-06-21 19:50:25 +0530

    feat: add new edit fields

--- src/components/Startups/StartupProfileHeader/ProfileEdit/EditForm/ProfileForm.jsx
+++ src/components/Startups/StartupProfileHeader/ProfileEdit/EditForm/ProfileForm.jsx
@@ -1,194 +1,241 @@
-import { useFormik } from 'formik'
-import axios from 'axios'
+import { useFormik } from "formik";
+import axios from "axios";
 
-import { profileSchemas } from './schemas/profileEditSchemas'
+import { profileSchemas } from "./schemas/profileEditSchemas";
 
-import TextInput from '../../../../TextInput/TextInput'
-import TextInput2 from '../../../../Dashboard/Forms/FormModal/Inputs/Text/TextInput'
-import FileUpload from '../../../../Dashboard/Forms/FormModal/Inputs/FileUpload/FileUpload'
+import TextInput from "../../../../TextInput/TextInput";
+import TextInput2 from "../../../../Dashboard/Forms/FormModal/Inputs/Text/TextInput";
+import FileUpload from "../../../../Dashboard/Forms/FormModal/Inputs/FileUpload/FileUpload";
 
-import styles from './form.module.scss'
+import styles from "./form.module.scss";
 
 export default function ProfileForm({ formClose }) {
-    const { values, errors, handleBlur, handleSubmit, handleChange, setFieldValue, setFieldError } = useFormik({
-        initialValues: {
-            username: '',
-            firstName: '',
-            lastName: '',
-            password: '',
-            confirm_password: '',
-            email: '',
-            profile_logo: '',
-            location: '',
-            website: '',
-            description: '',
-            twitter: '',
-            linkedin: '',
-            designation: '',
-            resume: '',
-            expertise: '',
-            domain: '',
-            company: '',
+  const {
+    values,
+    errors,
+    handleBlur,
+    handleSubmit,
+    handleChange,
+    setFieldValue,
+    setFieldError,
+  } = useFormik({
+    initialValues: {
+      username: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirm_password: "",
+      email: "",
+      contact_email: "",
+      profile_logo: "",
+      location: "",
+      website: "",
+      description: "",
+      twitter: "",
+      linkedin: "",
+      designation: "",
+      resume: "",
+      expertise: "",
+      domain: "",
+      company: "",
+      pitchdeck: "",
+      pitch_video: "",
+      stage: "",
+    },
+    onSubmit: (values, action) => {
+      console.log(values);
+      const requestObject = {
+        user: {
+          username: values.username.trim(),
+          password: values.password.trim(),
+          first_name: values.firstName.trim(),
+          last_name: values.lastName.trim(),
         },
-        onSubmit: (values, action) => {
-            console.log(values)
-            const requestObject = {
-                user: {
-                    username: values.username.trim(),
-                    password: values.password.trim(),
-                    first_name: values.firstName.trim(),
-                    last_name: values.lastName.trim(),
-                },
-                profile_logo: values.profile_logo,
-                google_email: values.email.trim(),
-                designation: values.designation.trim(),
-                linkedin: values.linkedin.trim(),
-                location: values.location.trim(),
-                description: values.description,
-                resume: values.resume.trim(),
-                sector_of_expertise: values.expertise,
-                domain_of_expertise: values.domain,
-                company_name: values.company,
-            }
-            axios.put('https://conquest-api.bits-dvm.org/api/users/profile/', requestObject, {
-                headers: {
-                    Authorization: `Bearer ${JSON.parse(localStorage.getItem('userData')).tokens.access}`
-                }
-            })
-                .then(res => {
-                    console.log(res)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-            action.resetForm()
-            formClose()
-        },
-        validationSchema: profileSchemas
-    })
+        profile_logo: values.profile_logo,
+        google_email: values.email.trim(),
+        contact_email: values.contact_email.trim(),
+        designation: values.designation.trim(),
+        linkedin: values.linkedin.trim(),
+        location: values.location.trim(),
+        description: values.description.trim(),
+        resume: values.resume.trim(),
+        sector_of_expertise: values.expertise,
+        domain_of_expertise: values.domain,
+        company_name: values.company.trim(),
+        pitch_deck: values.pitchdeck.trim(),
+        video_pitch: values.pitch_video.trim(),
+        stage: values.stage,
+      };
+      axios
+        .put(
+          "https://conquest-api.bits-dvm.org/api/users/profile/",
+          requestObject,
+          {
+            headers: {
+              Authorization: `Bearer ${
+                JSON.parse(localStorage.getItem("userData")).tokens.access
+              }`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      action.resetForm();
+      formClose();
+    },
+    validationSchema: profileSchemas,
+  });
 
-    return (
-        <main className={styles.form}>
-            <form
-                className={styles.wrapper}
-                onSubmit={handleSubmit}
-            >
-                <TextInput2
-                    name='username'
-                    heading="Change Username"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.username}
-                    error={errors.username}
-                    type='short'
-                />
-                <TextInput2
-                    name='firstName'
-                    heading="Change First Name"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.firstName}
-                    error={errors.firstName}
-                    type='short'
-                />
-                <TextInput2
-                    name='lastName'
-                    heading="Change Last Name"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.lastName}
-                    error={errors.lastName}
-                    type='short'
-                />
-                <TextInput2
-                    name='email'
-                    heading="Change Email"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.email}
-                    error={errors.email}
-                    type='short'
-                />
-                <TextInput2
-                    name='password'
-                    heading="Change Password"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.password}
-                    error={errors.password}
-                    type='short'
-                />
-                <TextInput2
-                    name='confirm_password'
-                    heading="Confirm Password"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.confirm_password}
-                    error={errors.confirm_password}
-                    type='short'
-                />
-                <FileUpload
-                    key='profile_logo'
-                    name='profile_logo'
-                    heading='Change Profile Logo'
-                    manualValue={setFieldValue}
-                    forceType='image'
-                />
-                <TextInput2
-                    name='company'
-                    heading="Change Company Name"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.company}
-                    error={errors.company}
-                    type='short'
-                />
-                <TextInput2
-                    name='location'
-                    heading="Change Location HQ"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.location}
-                    error={errors.location}
-                    type='short'
-                />
-                <TextInput2
-                    name='website'
-                    heading="Change Website Link"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.website}
-                    error={errors.website}
-                    type='link'
-                />
-                <TextInput2
-                    name='resume'
-                    heading="Share your resume as a google drive link"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.resume}
-                    error={errors.resume}
-                    type='link'
-                />
-                <TextInput2
-                    name='description'
-                    heading="Change Description"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.description}
-                    error={errors.description}
-                    type='long'
-                />
-                <TextInput2
-                    name='linkedin'
-                    heading="Change LinkedIn URL"
-                    changeFn={handleChange}
-                    blurFn={handleBlur}
-                    value={values.linkedin}
-                    error={errors.linkedin}
-                    type='link'
-                />
-                {/* <TextInput2
+  return (
+    <main className={styles.form}>
+      <form className={styles.wrapper} onSubmit={handleSubmit}>
+        <TextInput2
+          name="username"
+          heading="Change Username"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.username}
+          error={errors.username}
+          type="short"
+        />
+        <TextInput2
+          name="firstName"
+          heading="Change First Name"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.firstName}
+          error={errors.firstName}
+          type="short"
+        />
+        <TextInput2
+          name="lastName"
+          heading="Change Last Name"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.lastName}
+          error={errors.lastName}
+          type="short"
+        />
+        <TextInput2
+          name="email"
+          heading="Change Email"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.email}
+          error={errors.email}
+          type="short"
+        />
+        <TextInput2
+          name="contact_email"
+          heading="Change Contact Email"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.contact_email}
+          error={errors.contact_email}
+          type="short"
+        />
+        <TextInput2
+          name="password"
+          heading="Change Password"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.password}
+          error={errors.password}
+          type="short"
+        />
+        <TextInput2
+          name="confirm_password"
+          heading="Confirm Password"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.confirm_password}
+          error={errors.confirm_password}
+          type="short"
+        />
+        <FileUpload
+          key="profile_logo"
+          name="profile_logo"
+          heading="Change Profile Logo"
+          manualValue={setFieldValue}
+          forceType="image"
+        />
+        <TextInput2
+          name="company"
+          heading="Change Company Name"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.company}
+          error={errors.company}
+          type="short"
+        />
+        <TextInput2
+          name="location"
+          heading="Change Location HQ"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.location}
+          error={errors.location}
+          type="short"
+        />
+        <TextInput2
+          name="website"
+          heading="Change Website Link"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.website}
+          error={errors.website}
+          type="link"
+        />
+        <TextInput2
+          name="resume"
+          heading="Share your resume as a google drive link"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.resume}
+          error={errors.resume}
+          type="link"
+        />
+        <TextInput2
+          name="pitchdeck"
+          heading="Share your pitch deck as a google drive link"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.pitchdeck}
+          error={errors.pitchdeck}
+          type="link"
+        />
+        <TextInput2
+          name="pitch_video"
+          heading="Share your pitch video as a google drive link"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.pitch_video}
+          error={errors.pitch_video}
+          type="link"
+        />
+        <TextInput2
+          name="description"
+          heading="Change Description"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.description}
+          error={errors.description}
+          type="long"
+        />
+        <TextInput2
+          name="linkedin"
+          heading="Change LinkedIn URL"
+          changeFn={handleChange}
+          blurFn={handleBlur}
+          value={values.linkedin}
+          error={errors.linkedin}
+          type="link"
+        />
+        {/* <TextInput2
                     name='twitter'
                     heading="Change Twitter URL"
                     changeFn={handleChange}
@@ -197,8 +244,30 @@ export default function ProfileForm({ formClose }) {
                     error={errors.twitter}
                     type='link'
                 /> */}
-                <button type='submit' className={styles.submit}>Submit</button>
-            </form>
-        </main>
-    )
+        <div className={styles.inputGroup}>
+          <label htmlFor="stage">Select Stage</label>
+          <select
+            name="stage"
+            id="stage"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.stage}
+            className={styles.selectInput}
+          >
+            <option value="" label="Select stage" />
+            <option value="Pre-seed Stage" label="Pre-Seed" />
+            <option value="Seed Stage" label="Seed" />
+            <option value="Early Stage" label="Early" />
+            <option value="Growth Stage" label="Growth" />
+            <option value="Expansion Stage" label="Expansion" />
+            <option value="Exit Stage" label="Exit" />
+          </select>
+          {errors.stage && <div className={styles.error}>{errors.stage}</div>}
+        </div>
+        <button type="submit" className={styles.submit}>
+          Submit
+        </button>
+      </form>
+    </main>
+  );
 }
--- src/components/Startups/StartupProfileHeader/ProfileEdit/EditForm/schemas/profileEditSchemas.jsx
+++ src/components/Startups/StartupProfileHeader/ProfileEdit/EditForm/schemas/profileEditSchemas.jsx
@@ -1,18 +1,37 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const profileSchemas = Yup.object({
-    username: Yup.string(),
-    firstName: Yup.string(),
-    lastName: Yup.string(),
-    designation: Yup.string(),
-    company: Yup.string(),
-    email: Yup.string().email('Invalid email address'),
-    password: Yup.string().min(8, 'Password is too short - should be 8 chars minimum.'),
-    confirm_password: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
-    website: Yup.string().url('Invalid URL'),
-    resume: Yup.string().url('Invalid URL'),
-    description: Yup.string(),
-    location: Yup.string(),
-    twitter: Yup.string().url('Invalid URL').matches(/^(https:\/\/x\.com\/)/gi, { excludeEmptyString: true, message: 'Invalid Twitter / X.com URL' }),
-    linkedin: Yup.string().url('Invalid URL').matches(/^(https:\/\/www\.linkedin\.com\/company\/)/gi, { excludeEmptyString: true, message: 'Invalid LinkedIn URL' })
-})
+  username: Yup.string(),
+  firstName: Yup.string(),
+  lastName: Yup.string(),
+  designation: Yup.string(),
+  company: Yup.string(),
+  email: Yup.string().email("Invalid email address"),
+  contact_email: Yup.string().email("Invalid email address"),
+  password: Yup.string().min(
+    8,
+    "Password is too short - should be 8 chars minimum."
+  ),
+  confirm_password: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+  website: Yup.string().url("Invalid URL"),
+  resume: Yup.string().url("Invalid URL"),
+  description: Yup.string(),
+  location: Yup.string(),
+  twitter: Yup.string()
+    .url("Invalid URL")
+    .matches(/^(https:\/\/x\.com\/)/gi, {
+      excludeEmptyString: true,
+      message: "Invalid Twitter / X.com URL",
+    }),
+  linkedin: Yup.string()
+    .url("Invalid URL")
+    .matches(/^(https:\/\/www\.linkedin\.com\/company\/)/gi, {
+      excludeEmptyString: true,
+      message: "Invalid LinkedIn URL",
+    }),
+  pitchdeck: Yup.string().url("Invalid URL"),
+  pitch_video: Yup.string().url("Invalid URL"),
+});
