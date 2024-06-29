e9b1d8b8ebb6473761e249090c1398bf96acd80f
Animeshdj <animeshstick.567@gmail.com>
2024-06-30 02:29:33 +0530

    fix: change api endpoint

--- src/routes/Dashboard/Home/Home.jsx
+++ src/routes/Dashboard/Home/Home.jsx
@@ -55,16 +55,13 @@ export default function Home() {
   useEffect(() => {
     if (JSON.parse(localStorage.getItem("userData")).tokens) {
       axios
-        .get(
-          `https://conquest-api.bits-dvm.org/api/meetings/meetings/upcoming/`,
-          {
-            headers: {
-              Authorization: `Bearer ${
-                JSON.parse(localStorage.getItem("userData")).tokens.access
-              }`,
-            },
-          }
-        )
+        .get(`https://portal.conquest.org.in/api/meetings/meetings/upcoming/`, {
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("userData")).tokens.access
+            }`,
+          },
+        })
         .then((res) => {
           console.log(res.data);
 
