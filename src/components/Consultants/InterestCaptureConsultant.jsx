b08f6a9f91de742ce2c6fb65f19c6fafbb391c69
Chaitanya Jain <175200580+chaitanya-rahul-jain@users.noreply.github.com>
2024-07-13 18:58:40 +0530

    feat: call correct api on click on interest capture for consultant

--- src/components/Consultants/InterestCaptureConsultant.jsx
+++ src/components/Consultants/InterestCaptureConsultant.jsx
@@ -1,42 +1,56 @@
-import { useState, useContext } from 'react';
-import axios from 'axios';
-import styles from './InterestCaptureConsultant.module.scss';
+import { useState, useContext } from "react";
+import axios from "axios";
+import styles from "./InterestCaptureConsultant.module.scss";
 import { WebContext } from "../../store/website-context.jsx";
 
 export default function InterestCaptureConsultant({ data }) {
-    const [isDisabled, setIsDisabled] = useState(false);
-    const body = data.name;
-    const { displayMessage } = useContext(WebContext);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const body = data.name;
+  const { displayMessage } = useContext(WebContext);
 
-    const handleClick = () => {
-        if (JSON.parse(localStorage.getItem('userData'))) {
-            setIsDisabled(true);
-            axios.post('https://portal.conquest.org.in/api/users/consultant/interestcapture/', body, {
-                headers: {
-                    Authorization: `Bearer ${JSON.parse(localStorage.getItem("userData")).tokens.access}`
-                }
-            })
-                .then(res => {
-                    console.log(res);
-                    displayMessage('success', "Interest Captured Successfully", 2)
-                })
-                .catch(err => {
-                    console.log(err);
-                    setIsDisabled(false);
-                });
-        }
-    };
+  const handleClick = () => {
+    if (JSON.parse(localStorage.getItem("userData"))) {
+      setIsDisabled(true);
+      console.log(69, {
+        name: data.name.name,
+        type: "consultant",
+      });
+      axios
+        .post(
+          "https://portal.conquest.org.in/api/users/consultant-resource/interestcapture/",
+          {
+            name: data.name.name,
+            type: "consultant",
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${
+                JSON.parse(localStorage.getItem("userData")).tokens.access
+              }`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          displayMessage("success", "Interest Captured Successfully", 2);
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsDisabled(false);
+        });
+    }
+  };
 
-    return (
-        <a
-            className={styles.interest}
-            onClick={handleClick}
-            style={{ 
-                pointerEvents: isDisabled ? 'none' : 'auto', 
-                filter: isDisabled ? 'grayscale(1)' : 'none'
-            }}
-        >
-            Interest Capture
-        </a>
-    );
+  return (
+    <a
+      className={styles.interest}
+      onClick={handleClick}
+      style={{
+        pointerEvents: isDisabled ? "none" : "auto",
+        filter: isDisabled ? "grayscale(1)" : "none",
+      }}
+    >
+      Interest Capture
+    </a>
+  );
 }
